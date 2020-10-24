8b9ebfa1fbcd7c22ce78ac3a4dc56ec22307926a
Richard Butler <rpbutler@gmail.com>
2020-10-24 18:34:31 +0100

    Updates

---
+++ precache-manifest.1016f7e9856e307798d5e33fcac2096b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b443d64821c90c05ecf7aca0b10cd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e04dea1797b6d2e04f4",
+    "url": "/static/css/main.04f7b2bc.chunk.css"
+  },
+  {
+    "revision": "a785615e15bc8076c24b",
+    "url": "/static/js/2.36a1f9b3.chunk.js"
+  },
+  {
+    "revision": "63374af3dec0eaa4fdfa6d07d49b217d",
+    "url": "/static/js/2.36a1f9b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e04dea1797b6d2e04f4",
+    "url": "/static/js/main.e6b2ad36.chunk.js"
+  },
+  {
+    "revision": "2bdb1a8773e4d9951b6a",
+    "url": "/static/js/runtime-main.1d3a0fe4.js"
+  },
+  {
+    "revision": "b42c6b2e7f71f9389fb4f8ca54e5a060",
+    "url": "/static/media/about-photo.b42c6b2e.jpg"
+  },
+  {
+    "revision": "e3479ed30c676f15d5e581db53030fc7",
+    "url": "/static/media/deer.e3479ed3.svg"
+  },
+  {
+    "revision": "2a88fbe690e82512698bf342a99c280c",
+    "url": "/static/media/logo-blue.2a88fbe6.svg"
+  },
+  {
+    "revision": "6be0aef5ba94f1438f758836cb5f9f34",
+    "url": "/static/media/logo-insight.6be0aef5.svg"
+  }
+]);
